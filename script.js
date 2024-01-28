35ec95d55c6f64575f83ad198aec11dbefb9bb84
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2024-01-29 02:04:37 +0530

    uploading day 3 task file

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ request.send();
 request.onload = function(){
 var res = JSON.parse(request.response);
 console.log(res);
+// question 3. 
+for ( var i=0;i<=res.length; i++){
+    if(res[i].capital)
+    {
+        console.log(res[i].capital[0]+" "+res[i].area+ " "+res[i].flags+" "+res[i].region+" "+res[i].population+" "+res[i].subregion);
+    }
+    else{
+        console.log("invalid capital")
+    }
 }
+}
+
