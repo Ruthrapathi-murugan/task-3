c224e6d374bad8692aca8d30fd2eaa5ff1f38dcf
Ruthrapathi-murugan <153907596+Ruthrapathi-murugan@users.noreply.github.com>
2024-04-09 11:22:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,198 +1,180 @@
-//Do the below programs in anonymous function & IIFE
+// a) Print odd numbers in an array
 
+// Anonymous function
+(function(arr) {
+    arr.forEach(num => {
+        if (num % 2 !== 0) {
+            console.log(num);
+        }
+    });
+})([1, 2, 3, 4, 5]); // Output: 1, 3, 5
 
-// a)Print odd numbers in an array
-//anonymous function
-function odd(arr){
-    for(var i=0;i<arr.length;i++)
-    {
-      if(arr[i]%2!==0)
-    {
-      console.log(arr[i]);
-    }}}
-    var num=[1,2,3,4]
-    odd(num);
-
-//IIFE
-(function() {
-    function odd(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 !== 0) {
-                console.log(arr[i]);
-            }
+// IIFE
+(function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            console.log(arr[i]);
         }
     }
-    
-    var num = [1, 2, 3, 4];
-    odd(num);
-})();
+})([1, 2, 3, 4, 5]); // Output: 1, 3, 5
 
 
-//b.Convert all the strings to title caps in a string array
-//anonymous function
-var titleCase = function (str) {
-    str = str.toLowerCase().split(' ');
-    for (var i = 0; i < str.length; i++) {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-    } 
-    return str.join(' ');
-};
-var inputString = "this is ruthra";
-var result = titleCase(inputString);
-console.log(result); 
+// b) Convert all the strings to title caps in a string array
 
-//IIFE
-(function() {
-    var titleCase = function (str) {
-        str = str.toLowerCase().split(' ');
-        for (var i = 0; i < str.length; i++) {
-            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-        } 
-        return str.join(' ');
+// Anonymous function
+(function(arr) {
+    return arr.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+})(["ruthrapathi", "murugan"]); // Output: [ 'Ruthrapathi', 'Murugan' ]
+
+// IIFE
+(function(arr) {
+    var titleCase = arr.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+    console.log(titleCase);
+})(["ruthrapathi", "murugan"]); // Output: [ 'Ruthrapathi', 'Murugan' ]
+
+
+// c) Sum of all numbers in an array
+
+// Anonymous function
+(function(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+})([1, 2, 3, 4, 5]); // Output: 15
+
+// IIFE
+(function(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+})([1, 2, 3, 4, 5]); // Output: 15
+
+
+// d) Return all the prime numbers in an array
+
+// Anonymous function
+(function(arr) {
+    const isPrime = (num) => {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
     };
 
-    var inputString = "this is ruthra";
-    var result = titleCase(inputString);
-    console.log(result); 
-})();
+    return arr.filter(num => isPrime(num));
+})([1, 2, 3, 4, 5]); // Output: [2, 3, 5]
 
-
-//c.Sum of all numbers in an array
-
-//anonymous function
-function foo(...arr){
-    var sum = 0;
-    for(var i=0;i<arr.length;i++){
-    sum = sum+arr[i]
-    }
-    return sum;
-    
-    }
-    console.log(foo(12,13))
-    console.log(foo(121,131))
-    console.log(foo(12111,13111,1441,1551))
-
-
-
-//IIFE
-(function() {
-    function foo(...arr) {
-        var sum = 0;
-        for (var i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        return sum;
-    }
-
-    console.log(foo(12, 13));
-    console.log(foo(121, 131));
-    console.log(foo(12111, 13111, 1441, 1551));
-})();
-
-
-// d.Return all the prime numbers in an array
-
-//anonymous function
-var findPrimes = function(array) {
-    array = array.filter((number) => {
-        if (number < 2) return false; 
-        for (var i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i === 0) return false; 
+// IIFE
+(function(arr) {
+    const isPrime = (num) => {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
         }
         return true;
-    });
-    console.log(array);
-};
+    };
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-findPrimes(numbers);
+    var primes = arr.filter(num => isPrime(num));
+    console.log(primes);
+})([1, 2, 3, 4, 5]); // Output: [2, 3, 5]
 
-//IIFE
 
-(function(array) {
-    array = array.filter((number) => {
-        if (number < 2) return false; 
-        for (var i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i === 0) return false; 
-        }
-        return true;
-    });
-    console.log(array);
-})([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// e) Return all the palindromes in an array
 
-//Return all the palindromes in an array
+// Anonymous function
+(function(arr) {
+    const isPalindrome = (str) => {
+        const reversedStr = str.split('').reverse().join('');
+        return str === reversedStr;
+    };
 
-//anonymous function
-function isPalindrome(N) {
-    let str = "" + N;
-    let len = str.length;
-    for (let i = 0; i < parseInt(len / 2, 10); i++) {
-        if (str[i] != str[len - 1 - i])
-            return false;
+    return arr.filter(str => isPalindrome(str));
+})(["racecar", "hello", "level"]); // Output: ["racecar", "level"]
+
+// IIFE
+(function(arr) {
+    const isPalindrome = (str) => {
+        const reversedStr = str.split('').reverse().join('');
+        return str === reversedStr;
+    };
+
+    var palindromes = arr.filter(str => isPalindrome(str));
+    console.log(palindromes);
+})(["racecar", "hello", "level"]); // Output: ["racecar", "level"]
+
+
+// f) Return median of two sorted arrays of the same size.
+
+// Anonymous function
+(function(arr1, arr2) {
+    const mergeArrays = [...arr1, ...arr2];
+    mergeArrays.sort((a, b) => a - b);
+    const length = mergeArrays.length;
+    if (length % 2 === 0) {
+        return (mergeArrays[length / 2 - 1] + mergeArrays[length / 2]) / 2;
+    } else {
+        return mergeArrays[Math.floor(length / 2)];
     }
-    return true;
-}
+})([1, 3, 5], [2, 4, 6]); // Output: 3.5
 
-console.log(isPalindrome(12321)); // Output: true
-console.log(isPalindrome(12345)); // Output: false
-
-
-
-
-//anonymous function
-
-(function(N) {
-    let str = "" + N;
-    let len = str.length;
-    for (let i = 0; i < parseInt(len / 2, 10); i++) {
-        if (str[i] != str[len - 1 - i])
-            return false;
+// IIFE
+(function(arr1, arr2) {
+    const mergeArrays = [...arr1, ...arr2];
+    mergeArrays.sort((a, b) => a - b);
+    const length = mergeArrays.length;
+    var median;
+    if (length % 2 === 0) {
+        median = (mergeArrays[length / 2 - 1] + mergeArrays[length / 2]) / 2;
+    } else {
+        median = mergeArrays[Math.floor(length / 2)];
     }
-    console.log(true);
-    return true;
-})(12321);
+    console.log(median);
+})([1, 3, 5], [2, 4, 6]); // Output: 3.5
 
 
-//F.Return median of two sorted arrays of the same size.
-//anonymous function
+// g) Remove duplicates from an array
 
-
-//IIFE function
-
-
-
-
-//G.Remove duplicates from an array
-//anonymous function
-var removeDuplicates = function(array) {
-    var dup = [...new Set(array)];
-    console.log(dup);
-};
-removeDuplicates([1, 1, 2, 3, 4]); // Output: [1, 2, 3, 4]
-
-
-
-
-//IIFE function
-(function(array) {
-    let dup = [...new Set(array)];
-    console.log(dup);
+// Anonymous function
+(function(arr) {
+    return [...new Set(arr)];
 })([1, 1, 2, 3, 4]); // Output: [1, 2, 3, 4]
 
-// h.Rotate an array by k times
- 
-//anonymous function
-function rotateArray(array, k) {
-    k = k % array.length;
+// IIFE
+(function(arr) {
+    var uniqueArray = [...new Set(arr)];
+    console.log(uniqueArray);
+})([1, 1, 2, 3, 4]); // Output: [1, 2, 3, 4]
+
+
+// h) Rotate an array by k times
+
+// Anonymous function
+(function(arr, k) {
+    k = k % arr.length;
     if (k < 0) {
-        k += array.length;
+        k += arr.length;
     }
 
-    reverse(array, 0, array.length - k - 1);
-    reverse(array, array.length - k, array.length - 1);
-    reverse(array, 0, array.length - 1);
-}
+    reverse(arr, 0, arr.length - k - 1);
+    reverse(arr, arr.length - k, arr.length - 1);
+    reverse(arr, 0, arr.length - 1);
+})([1, 2, 3, 4], 2); // Output: [3, 4, 1, 2]
 
+// IIFE
+(function(arr, k) {
+    k = k % arr.length;
+    if (k < 0) {
+        k += arr.length;
+    }
+
+    reverse(arr, 0, arr.length - k - 1);
+    reverse(arr, arr.length - k, arr.length - 1);
+    reverse(arr, 0, arr.length - 1);
+    console.log(arr);
+})([1, 2, 3, 4], 2); // Output: [3, 4, 1, 2]
+
+// Helper function for rotating array
 function reverse(array, li, ri) {
     while (li < ri) {
         let temp = array[li];
@@ -203,65 +185,56 @@ function reverse(array, li, ri) {
         ri--;
     }
 }
-let arr = [1, 2, 3, 4];
-rotateArray(arr, 2);
-console.log(arr); // Output: [3, 4, 1, 2]
+
+// Arrow function implementations
+
+// Print odd numbers in an array
+const printOddNumbers = (arr) => {
+    arr.forEach(num => {
+        if (num % 2 !== 0) {
+            console.log(num);
+        }
+    });
+};
+printOddNumbers([1, 2, 3, 4, 5]); // Output: 1, 3, 5
 
 
-    //Do the below programs in arrow functions. 
-
-    //a.Print odd numbers in an array
-
-    var OddNumber = (arr) => {
-        arr.forEach(num => {
-            if (num % 2 !== 0) {
-                console.log(num);
-            }
-        });
-    };
-         OddNumber([1, 2, 3, 4, 5]); // Output: 1, 3, 5
+// Convert all the strings to title caps in a string array
+const convertToTitleCaps = (arr) => {
+    return arr.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+};
+console.log(convertToTitleCaps(["hello", "world"])); // Output: ["Hello", "World"]
 
 
-        //b. Convert all the strings to title caps in a string array
-
-        var Title = (arr) => {
-            return arr.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
-        };
-        console.log(Title(["ruthrapathi", "murugan"])); // Output: [ 'Ruthrapathi', 'Murugan' ]
-
-        //c.Sum of all numbers in an array
-
-        var sumArray = (arr) => {
-            return arr.reduce((acc, curr) => acc + curr, 0);
-        };
-        console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+// Sum of all numbers in an array
+const sumArray = (arr) => {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+};
+console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
 
 
-        //d.Return all the prime numbers in an array
+// Return all the prime numbers in an array
+const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
 
-        var Prime = (num) => {
-            if (num <= 1) return false;
-            for (let i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i === 0) return false;
-            }
-            return true;
-        };
-        
-        const getPrimeNumbers = (arr) => {
-            return arr.filter(num => Prime(num));
-        };
-        console.log(getPrimeNumbers([1, 2, 3, 4, 5])); // Output: [2, 3, 5]
+const getPrimeNumbers = (arr) => {
+    return arr.filter(num => isPrime(num));
+};
+console.log(getPrimeNumbers([1, 2, 3, 4, 5])); // Output: [2, 3, 5]
 
-        //e.Return all the palindromes in an array
 
-        var isPalindrome = (str) => {
-            const reversedStr = str.split('').reverse().join('');
-            return str === reversedStr;
-        };
-        
-        const getPalindromes = (arr) => {
-            return arr.filter(str => isPalindrome(str));
-        };
-        
-        console.log(getPalindromes(["racecar", "hello", "level"])); // Output: ["racecar", "level"]
+// Return all the palindromes in an array
+const isPalindrome = (str) => {
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+};
 
+const getPalindromes = (arr) => {
+    return arr.filter(str => isPalindrome(str));
+};
+console.log(getPalindromes(["level", "hello", "racecar"])); // Output: ["level", "racecar"]
